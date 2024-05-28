03345d5c7fcb8a7a9778e32a439e2892be3801b2
lumasia stancelaus <lumasiastancelaus@gmail.com>
2024-05-28 19:58:38 +0300

    button

--- frontend/src/Homepage.js
+++ frontend/src/Homepage.js
@@ -18,8 +18,11 @@ class Homepage extends Component {
   render() {
     return (
       <div className='relative min-h-screen bg-cover bg-center' style={{ backgroundImage: "url('/SOFTWARE ENGINEER (2).png')" }}>
-        <h1 className='font-raleway text-4xl px-6 py-4 font-semibold absolute top-0 left-0'>Payee</h1>
-        <div className="font-raleway w-full flex justify-center pt-3">
+        <h1 className='font-mons text-4xl px-6 py-4 font-semibold absolute top-0 left-0'>Payee</h1>
+        <button className=" font-raleway absolute top-0 right-10 m-5 px-4 py-2 bg-white text-black border border-orange-500 hover:bg-orange-300 transition duration-200 shadow-sm rounded-md">
+          Get Started
+        </button>
+        <div className="font-raleway w-full flex justify-center pt-4">
           {this.renderOpts()}
         </div>
       </div>
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.js"],
+  theme: {
+    extend: {
+      fontFamily: {
+        raleway: 'Raleway',
+        mons: 'Montserrat Alternates'
+      }
+    },
+  },
+  plugins: [],
+}
+
