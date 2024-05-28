b90235d0b62429be3eb19b92f4950b133dd796b1
lumasia stancelaus <lumasiastancelaus@gmail.com>
2024-05-29 01:01:31 +0300

    done

--- frontend/src/Homepage.js
+++ frontend/src/Homepage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+
 const options = ['Products', 'About', 'FAQ'];
 
 class Homepage extends Component {
@@ -18,12 +19,38 @@ class Homepage extends Component {
   render() {
     return (
       <div className='relative min-h-screen bg-cover bg-center' style={{ backgroundImage: "url('/SOFTWARE ENGINEER (2).png')" }}>
-        <h1 className='font-mons text-4xl px-6 py-4 font-semibold absolute top-0 left-0'>Payee</h1>
-        <button className=" font-raleway absolute top-0 right-10 m-5 px-4 py-2 bg-white text-black border border-orange-500 hover:bg-orange-300 transition duration-200 shadow-sm rounded-md">
+        <div className='absolute top-0 left-0'>
+          <h1 className='font-mons text-4xl px-6 py-5 font-semibold'>Payee</h1>
+          <div className='mt-4 px-6'>
+            <img
+              src="/igal-ness-9wY2ofzQ9Us-unsplash.jpg"
+              alt="Description"
+              className='rounded-lr-custom w-full sm:w-2/3 md:w-1/2 lg:w-1/4 h-auto'
+            />
+          </div>
+          <div className='mt-4 px-6'>
+            <img
+              src="/rupixen-Q59HmzK38eQ-unsplash.jpg"
+              alt="Second Description"
+              className='rounded-lr-custom-second w-full sm:w-2/3 md:w-1/2 lg:w-1/4 h-auto'
+            />
+          </div>
+        </div>
+        <button className="font-raleway absolute top-0 right-10 md:right-10 lg:right-20 m-5 px-4 py-2 bg-white text-black border border-orange-500 hover:bg-orange-300 transition duration-200 shadow-md rounded-md">
           Get Started
         </button>
         <div className="font-raleway w-full flex justify-center pt-4">
           {this.renderOpts()}
+        </div>
+        <div className='flex justify-center mt-8'>
+          <h1 className='font-tour pt-10 text-2xl text-center md:text-left ml-10'>
+            Money Come, Money Go?
+          </h1>
+        </div>
+        <div className='w-full flex justify-center items-center mt-20 ml-12'>
+          <h1 className='font-tour font-semibold pt-20 text-4xl text-center md:text-center lg:text-right pl-12'>
+            Your AI-powered financial manager<br />
+          </h1>
         </div>
       </div>
     );
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,10 +5,33 @@ module.exports = {
     extend: {
       fontFamily: {
         raleway: 'Raleway',
-        mons: 'Montserrat Alternates'
-      }
+        mons: 'Montserrat Alternates',
+        tour: 'Autour One'
+      },
+      borderRadius: {
+        'custom-left': '1rem',  // Medium rounded corner
+        'custom-right': '9999px',
+        'custom-second-left': '0.5rem', // Slightly rounded corner for second image
+        'custom-second-right': '50%',  // Fully rounded corner
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        '.rounded-lr-custom': {
+          'border-top-left-radius': '250px',
+          'border-bottom-left-radius': '500px',
+          'border-top-right-radius': '500px',
+          'border-bottom-right-radius': '250px',
+        },
+        '.rounded-lr-custom-second': {
+          'border-top-left-radius': '500px',
+          'border-bottom-left-radius': '250px',
+          'border-top-right-radius': '250px',
+          'border-bottom-right-radius': '500px',
+        },
+      });
+    },
+  ],
 }
-
