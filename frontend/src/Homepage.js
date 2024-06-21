b81b9914cc5fdbed484c3fe53977071c491c356d
lumasia stancelaus <lumasiastancelaus@gmail.com>
2024-06-22 00:05:56 +0300

    ok

--- frontend/src/Homepage.js
+++ frontend/src/Homepage.js
@@ -32,30 +32,14 @@ class Homepage extends Component {
     const { showLogin } = this.state;
 
     return (
-      <div className='relative min-h-screen bg-cover bg-center' style={{ backgroundImage: "url('/SOFTWARE ENGINEER (2).png')" }}>
+      <div className='bg-gray-100 min-h-screen'>
         <div className='absolute top-0 left-0'>
-          <h1 className='font-mons text-4xl px-6 py-5 font-semibold'>Payee</h1>
+          <h1 className='font-mons text-4xl px-6 py-5 font-semibold text-orange-500'>Payee</h1>
           <div className='mt-4 px-6'>
-            <div className='relative w-32 h-32 sm:w-40 sm:h-40 md:w-48 md:h-48 lg:w-56 lg:h-56 overflow-hidden'>
-              <img
-                src="/igal-ness-9wY2ofzQ9Us-unsplash.jpg"
-                alt="Description"
-                className='absolute inset-0 w-full h-full object-cover rounded-lr-custom'
-              />
-            </div>
-          </div>
-          <div className='mt-4 px-6'>
-            <div className='relative w-32 h-32 sm:w-40 sm:h-40 md:w-48 md:h-48 lg:w-56 lg:h-56 overflow-hidden'>
-              <img
-                src="/rupixen-Q59HmzK38eQ-unsplash.jpg"
-                alt="Second Description"
-                className='absolute inset-0 w-full h-full object-cover rounded-lr-custom-second'
-              />
-            </div>
-          </div>
+           </div>
         </div>
         <button
-          className="font-raleway absolute top-0 right-10 md:right-10 lg:right-20 m-5 px-4 py-2 bg-white text-black border border-orange-500 hover:bg-orange-300 transition duration-200 shadow-md rounded-md"
+          className=" rounded-lg font-raleway absolute top-0 right-10 md:right-10 lg:right-20 m-5 px-4 py-2 bg-orange-200 text-black border border-orange-500 hover:bg-orange-300 transition duration-200 shadow-md"
           onClick={this.handleLoginClick}
         >
           Log in ?
@@ -63,20 +47,33 @@ class Homepage extends Component {
         <div className="font-raleway w-full flex justify-center pt-4">
           {this.renderOpts()}
         </div>
-        <div className='flex justify-center mt-8'>
-          <h1 className='font-tour pt-10 text-2xl text-center md:text-left ml-10'>
-            Money Come, Money Go?
+        <div className='flex justify-center px-12 mx-12'>
+          <h1 className='font-raleway py-12 text-4xl font-semibold text-center px-12 mx-12'>
+            The fastest way to manage you finances & budgets with AI-powered solutions
           </h1>
         </div>
-        <div className='flex w-full justify-center items-center my-20'>
-          <h1 className='font-tour font-semibold py-10 text-4xl text-center md:text-center lg:text-right lg:ml-20'>
-            Your AI-powered financial manager
-          </h1>
+        <div className='font-raleway flex justify-center items-center'>
+          <h2>Manage your income by the click of a button</h2>
         </div>
-        <div className='absolute bottom-20 w-full flex justify-center'>
-          <button className='font-raleway px-6 py-3 bg-orange-300 text-black rounded-md transition duration-200 hover:bg-gradient-to-r from-orange-300 to-orange-500'>
-            Get Started
+        <div className='py-3 w-full flex justify-center my-3'>
+          <button className='shadow-md font-raleway px-6 py-3 bg-orange-300 text-black rounded-full transition duration-200 hover:bg-gradient-to-r from-orange-300 to-orange-500'>
+            Try it out {'>'} 
           </button>
+        </div>
+        <div className='py-12 my-12 mx-5'>
+          <h1 className='font-raleway font-semibold flex justify-center text-3xl'>Our Products</h1>
+          <div className='flex justify-center space-x-12 py-7'>
+          <div className='w-1/4 bg-orange-300 rounded-md shadow-lg'>
+            me
+          </div>
+          <div className='w-1/4 bg-orange-300 rounded-md shadow-lg'>
+            me
+          </div>
+          <div className='w-1/4 bg-orange-300 rounded-md shadow-lg'>
+            me
+          </div>
+          </div>
+          
         </div>
         {showLogin && (
           <div className="absolute inset-0 bg-black bg-opacity-70 flex justify-center items-center">
