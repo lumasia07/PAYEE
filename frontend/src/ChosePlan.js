a8694cbe78684d4e48a36a584815d93cd1d6d823
lumasia stancelaus <lumasiastancelaus@gmail.com>
2024-06-26 10:21:15 +0300

    plan button

--- frontend/src/ChosePlan.js
+++ frontend/src/ChosePlan.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class ChosePlan extends Component {
   render() {
@@ -11,25 +12,31 @@ class ChosePlan extends Component {
             <h2 className='text-2xl font-semibold mb-4 text-gray-700'>Home Plan</h2>
             <p className='mb-4 text-gray-600'>Ideal for home budgets & billing</p>
             <p className='text-lg font-bold mb-6 text-gray-800'>$Free/month</p>
-            <button className='bg-orange-500 text-white py-2 px-4 rounded hover:bg-blue-600 focus:outline-none focus:border-orange-500'>
-              Select Plan
-            </button>
+            <Link to="/register">
+              <button className='bg-orange-500 text-white py-2 px-4 rounded hover:bg-blue-600 focus:outline-none focus:border-orange-500'>
+                Select Plan
+              </button>
+            </Link>
           </div>
           <div className='plan-card bg-white border rounded-lg p-6 text-center shadow-lg transition-transform transform hover:scale-105'>
             <h2 className='text-2xl font-semibold mb-4 text-gray-700'>Business Plan</h2>
-            <p className='mb-4 text-gray-600'>For professionals & bussiness needs</p>
+            <p className='mb-4 text-gray-600'>For professionals & business needs</p>
             <p className='text-lg font-bold mb-6 text-gray-800'>$Free/month</p>
-            <button className='bg-orange-500 text-white py-2 px-4 rounded hover:bg-blue-600 focus:outline-none focus:border-orange-500'>
-              Select Plan
-            </button>
+            <Link to="/register">
+              <button className='bg-orange-500 text-white py-2 px-4 rounded hover:bg-blue-600 focus:outline-none focus:border-orange-500'>
+                Select Plan
+              </button>
+            </Link>
           </div>
           <div className='plan-card bg-white border rounded-lg p-6 text-center shadow-lg transition-transform transform hover:scale-105'>
             <h2 className='text-2xl font-semibold mb-4 text-gray-700'>Enterprise Plan</h2>
             <p className='mb-4 text-gray-600'>Best for large organizations & institutions</p>
             <p className='text-lg font-bold mb-6 text-gray-800'>$100/month</p>
-            <button className='bg-orange-500 text-white py-2 px-4 rounded hover:bg-blue-600 focus:outline-none focus:border-orange-500'>
-              Select Plan
-            </button>
+            <Link to="/register">
+              <button className='bg-orange-500 text-white py-2 px-4 rounded hover:bg-blue-600 focus:outline-none focus:border-orange-500'>
+                Select Plan
+              </button>
+            </Link>
           </div>
         </div>
       </div>
